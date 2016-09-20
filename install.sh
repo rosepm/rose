675d3d3b38be77287f3df0c9d15c11313a645955
Sam Hart <hartsn@gmail.com>
2016-09-20 00:25:49 -0400

    begin to flesh out install

--- install.sh
+++ install.sh
@@ -2,25 +2,48 @@
 
 # rose install tool
 
+unset LIBPATH BINPATH || true
+
 PROGNAME=${0##*/}
 
 MY_CWD=`pwd`
 
 usage()
 {
-    cat <<EOF
-Usage: $PROGNAME [OPTION]
-Where [OPTION] is one of the following
-    --lib=LIBPATH/        Installs the libraries to LIBPATH/
+  cat <<EOF
+  Usage: $PROGNAME [OPTION]
+  Where [OPTION] is one of the following:
 
-    --bin=BINPATH/        Installs the binary to BINPATH/
+  --lib=LIBPATH/        Installs the libraries to LIBPATH/
 
-If run with nothing, will install to user's home directory.
+  --bin=BINPATH/        Installs the binary to BINPATH/
+
+  --help                Display this help message
+
+  If run with nothing, will install to user's home directory.
 EOF
 }
 
-if [ "$1" = "" ]; then
-    usage
-    exit 0
-fi
+for i in "$@"
+do
+  case $i in
+    --lib=*)
+      LIBPATH="${i#*=}"
+      shift
+      ;;
+    --bin=*)
+      BINPATH="${i#*=}"
+      shift
+      ;;
+    -h|--help)
+      usage
+      exit 0
+      shift
+      ;;
+    *)
+      usage 0
+      shift
+      ;;
+  esac
+done
 
