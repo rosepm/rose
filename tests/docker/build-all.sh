91482caecc6fd3acbe91c52a66aab66004253db3
Sam Hart <hartsn@gmail.com>
2016-10-27 13:21:45 -0400

    basics of a docker thang- also HAPPY BDAY COMMIT

---
+++ tests/docker/build-all.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+DIRS=(*/)
+
+echo ">> Creating test images"
+echo ">>---------------------------------------"
+
+for d in "${DIRS[@]}"
+do
+  img="${d%/}"
+  echo "- Creating image '${img}'"
+  cd $d
+  docker build -t="${img}" .
+  cd .. 
+  echo "+ ---------------------------------DONE: ${img}\n"
+done
